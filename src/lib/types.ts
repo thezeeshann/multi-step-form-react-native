e4a1f0d9b5690ec3f6605540cd35e84238de4649
thezeeshann <info.zeeshann@gmail.com>
2024-12-22 04:39:39 +0530

    refactor: added mote types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -7,14 +7,18 @@ export const PersonalInfoSchema = z.object({
   address: z.string().min(1, { message: "Please provide your address!" }),
   city: z.string().min(1, { message: "City is required!" }),
   postcode: z.string().min(1, { message: "Postal code is required!" }),
+  country: z.string().length(2),
   phone: z.string().min(1, { message: "Phone is required!" }),
+  birthDate: z.date(),
 });
 
 export type PersonalInfo = z.infer<typeof PersonalInfoSchema>;
 
 export const PaymentInfoSchema = z.object({
   cardNumber: z.string().min(1),
-  expireDate: z.string().regex(/^(0[1-9]|1[0-2])\/?([0-9]{2})$/,"Please use the MM/YY format"),
+  expireDate: z
+    .string()
+    .regex(/^(0[1-9]|1[0-2])\/?([0-9]{2})$/, "Please use the MM/YY format"),
   cvv: z.coerce.number().min(1),
 });
 
